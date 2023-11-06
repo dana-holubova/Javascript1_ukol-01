e0d349b4d838dc43f82c64d3bafba8887eb36cf2
Dana Holubová <danmich@email.cz>
2023-11-06 08:57:35 +0100

    Přidání fce toLowerCase.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const krestniJmeno = prompt("Zadejte vaše křestní jméno bez diakritiky: ")
 const prijmeni = prompt("Zadejte vaše příjmení bez diakritiky: ")
 
-krestniJmenoZkratka = krestniJmeno.trim().slice(0, 3)
-prijmeniZkratka = prijmeni.trim().slice(0, 4)
+//krestniJmenoZkratka = krestniJmeno.toLowerCase().trim().slice(0, 3)
+krestniJmenoZkratka = krestniJmeno.toLowerCase().trim().slice(0, 3)
+//prijmeniZkratka = prijmeni.toLowerCase.trim().slice(0, 4)
+prijmeniZkratka = prijmeni.toLowerCase().trim().slice(0, 4)
 
 console.log(krestniJmenoZkratka)
 console.log(prijmeniZkratka)
