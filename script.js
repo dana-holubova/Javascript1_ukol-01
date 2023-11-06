deaa0251aba8d32f4b2d836592c452233d5f348f
Dana Holubová <danmich@email.cz>
2023-11-06 09:13:22 +0100

    Doplněna deklarace k proměnným.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const krestniJmeno = prompt("Zadejte vaše křestní jméno bez diakritiky: ")
 const prijmeni = prompt("Zadejte vaše příjmení bez diakritiky: ")
 
-//krestniJmenoZkratka = krestniJmeno.toLowerCase().trim().slice(0, 3)
-krestniJmenoZkratka = krestniJmeno.toLowerCase().trim().slice(0, 3)
-//prijmeniZkratka = prijmeni.toLowerCase.trim().slice(0, 4)
-prijmeniZkratka = prijmeni.toLowerCase().trim().slice(0, 4)
+const krestniJmenoZkratka = krestniJmeno.toLowerCase().trim().slice(0, 3)
+
+const prijmeniZkratka = prijmeni.toLowerCase().trim().slice(0, 4)
 
 console.log(krestniJmenoZkratka)
 console.log(prijmeniZkratka)
